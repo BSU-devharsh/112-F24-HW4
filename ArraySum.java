f44f3ad4d2d048b2eeb87a4d2f40884a4868d1d4
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-11-12 13:57:34 -0500

    Create ArraySum.java

---
+++ ArraySum.java
@@ -0,0 +1,16 @@
+// Write your name here
+
+import java.util.Scanner;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements: ");
+        int size = scanner.nextInt();
+        
+        // Write your code here
+        
+        scanner.close();
+    }
+}
